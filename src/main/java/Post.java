d28a10f8d479d9b1e506cdd8f40085f393ae8e1a
Netology QA <obgynkostina@gmail.com>
2023-03-22 16:46:24 +0300

    modified

---
+++ src/main/java/Post.java
@@ -0,0 +1,21 @@
+public class Post {
+        public String name;
+        public String surname;
+        public String patronymic;
+        public String passport;
+        public String phone;
+        boolean subscription;
+        public FormDate birthday;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
